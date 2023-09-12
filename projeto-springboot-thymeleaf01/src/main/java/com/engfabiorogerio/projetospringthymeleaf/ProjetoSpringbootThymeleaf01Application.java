cf7bbdda466aceea13f3a20faa27b1b98da989fe
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-09-12 12:49:55 -0300

    Initial Commit

---
+++ projeto-springboot-thymeleaf01/src/main/java/com/engfabiorogerio/projetospringthymeleaf/ProjetoSpringbootThymeleaf01Application.java
@@ -0,0 +1,13 @@
+package com.engfabiorogerio.projetospringthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootThymeleaf01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootThymeleaf01Application.class, args);
+	}
+
+}
